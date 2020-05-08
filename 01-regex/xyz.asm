2eee644853769c02d8f0b77b5b553a6877659564
Austin Weary <32180534+Slayerrunes@users.noreply.github.com>
2020-05-08 18:26:32 -0400

    Clearing up some files

--- 01-regex/xyz.asm
+++ 01-regex/xyz.asm
@@ -2,20 +2,13 @@ default rel
 section .text
 global main
 main:
-push qword 0
+push qword 1
+push qword 2
 pop rax
-cmp rax, 0
-je lbl0
-push qword 34
-pop rax
-ret
-jmp lbl1
-lbl0:
-push qword 78
-pop rax
-ret
-lbl1:
-push qword 56
+cmp [rsp],rax
+sete  al
+movzx qword rax, al
+mov [rsp], rax
 pop rax
 ret
 ret
