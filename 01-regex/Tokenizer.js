f5a75bc6905889ce8b0c353dbf4f454998b1aa43
Austin Weary <32180534+Slayerrunes@users.noreply.github.com>
2020-05-08 19:46:45 -0400

    Recommitting the Tokenizer code.

---
+++ 01-regex/Tokenizer.js
@@ -0,0 +1,84 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Token_1 = require("./Token");
+class Tokenizer {
+    constructor(grammar) {
+        this.currentLine = 1;
+        this.previousList = [];
+        this.idx = 0; //index of next unparsed char in inputData
+        this.grammar = grammar;
+        let addWhitespace = true;
+        let addComment = true;
+        for (let i = 0; i < this.grammar.terminals.length; ++i) {
+            if (this.grammar.terminals[i][0] == "WHITESPACE")
+                addWhitespace = false;
+            if (this.grammar.terminals[i][0] == "COMMENT")
+                addComment = false;
+        }
+        if (addWhitespace)
+            this.grammar.terminals.push(["WHITESPACE", new RegExp("\\s+")]);
+        if (addComment)
+            this.grammar.terminals.push(["COMMENT", new RegExp("/\\*(.|\\n)*?\\*/")]);
+    }
+    setInput(inputData) {
+        this.inputData = inputData;
+        this.currentLine = 1;
+        this.idx = 0;
+        console.log(inputData);
+    }
+    next() {
+        if (this.idx >= this.inputData.length) {
+            //special "end of file" metatoken
+            return new Token_1.Token("$", this.currentLine, undefined);
+        }
+        for (let i = 0; i < this.grammar.terminals.length; ++i) {
+            let terminal = this.grammar.terminals[i];
+            let sym = terminal[0];
+            let rex = new RegExp(terminal[1], "y"); //RegExp
+            rex.lastIndex = this.idx; //tell where to start searching
+            let m = rex.exec(this.inputData); //do the search
+            if (m) {
+                //m[0] contains matched text as string
+                let lexeme = m[0];
+                this.idx += lexeme.length;
+                let tmp = this.currentLine;
+                this.currentLine += lexeme.split('\n').length - 1;
+                if (sym !== "WHITESPACE" && sym !== "COMMENT") {
+                    //return new Token using sym, lexeme, and line number
+                    this.cur = new Token_1.Token(sym, tmp, lexeme);
+                    this.previousList.push(this.cur);
+                    if (this.previousList.length > 2) {
+                        this.previousList.shift();
+                    }
+                    return this.cur;
+                }
+                else {
+                    //skip whitespace and get next real token
+                    return this.next();
+                }
+            }
+        }
+        //no match; syntax error
+        throw new Error("Syntax Error!!!");
+    }
+    peek() {
+        let tmpCur = this.cur;
+        let tmpLine = this.currentLine;
+        let tmpList = this.previousList;
+        let tmpIdx = this.idx;
+        let tmpNext = this.next();
+        this.cur = tmpCur;
+        this.currentLine = tmpLine;
+        this.previousList = tmpList;
+        this.idx = tmpIdx;
+        return tmpNext;
+    }
+    previous() {
+        if (this.previousList.length < 2) {
+            return undefined;
+        }
+        return this.previousList[this.previousList.length - 2];
+    }
+}
+exports.Tokenizer = Tokenizer;
+//# sourceMappingURL=Tokenizer.js.map
--- 01-regex/xyz.asm
+++ 01-regex/xyz.asm
@@ -3,12 +3,28 @@ section .text
 global main
 main:
 push qword 1
-push qword 2
+mov rax, __float64__(2.999)
+push rax
+movq xmm0, [rsp]
+add rsp, 8
+roundsd xmm0, xmm0, 0xb
+cvtsd2si rax, xmm0
+push rax
+pop rbx
 pop rax
-cmp [rsp],rax
-sete  al
-movzx qword rax, al
-mov [rsp], rax
+add rax, rbx
+push rax
+mov rax, __float64__(3.5)
+push rax
+movq xmm0, [rsp]
+add rsp, 8
+roundsd xmm0, xmm0, 0xb
+cvtsd2si rax, xmm0
+push rax
+pop rbx
+pop rax
+add rax, rbx
+push rax
 pop rax
 ret
 ret
